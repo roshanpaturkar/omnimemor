68ecc2b1034083346714fc68639e25e61f0e8e6b
Roshan Paturkar <paturkarr@gmail.com>
2022-08-27 19:14:05 +0530

    loading animation added

--- src/pages/LoadingPage.js
+++ src/pages/LoadingPage.js
@@ -1,6 +1,9 @@
 import { useEffect } from 'react';
+import styled from "styled-components";
+import ReactLoading from 'react-loading';
 import useUserStore from '../store/userStore';
 import { useNavigate } from 'react-router-dom';
+import loadingImage from '../assets/images/loading.jpg';
 
 const LoadingPage = () => {
   const navigate = useNavigate();
@@ -14,7 +17,24 @@ const LoadingPage = () => {
       navigate('/login');
     }
   }, [userToken, getUserDetails, navigate]);
-  return <h2>Loading Page</h2>;
-}
+  return (
+    <MainWrapper>
+      <ReactLoading type='bars' color='#fff' height={250} width={250} />
+    </MainWrapper>
+  );
+};
+
+const MainWrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${loadingImage});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
 
 export default LoadingPage;
