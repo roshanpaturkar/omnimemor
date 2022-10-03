8d0b13af998bba1e38cc7d0265d7e7b33c7196eb
Roshan Paturkar <paturkarr@gmail.com>
2022-10-04 01:51:41 +0530

    password change feature implemented

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,19 +1,37 @@
+import axios from "axios";
 import { useState } from "react";
 import styled from "styled-components";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import useUserStore from "../store/userStore";
 import useSidebarStateManagerStore from "../store/sidebarStateManagerStore";
+import { customLogger, httpErrorLogger } from "../utils/logsManager";
 
 const Sidebar = () => {
     const { userDetails, isUserLoggedIn, logout, updateAvatar, updateName } = useUserStore();
-    const { isUpdateProfileOpen, setUpdateProfileOpen, isUpdateNameOpen, setUpdateNameOpen } = useSidebarStateManagerStore();
+    const { isUpdateProfileOpen, setUpdateProfileOpen, isUpdateNameOpen, setUpdateNameOpen, isUpdatePasswordOpen, setUpdatePasswordOpen } = useSidebarStateManagerStore();
 
-    const [source, setSource] = useState(userDetails? userDetails.avatar : '');
-    const [name, setName] = useState(userDetails? userDetails.name : '');
+    const [source, setSource] = useState(userDetails ? userDetails.avatar : '');
+    const [name, setName] = useState(userDetails ? userDetails.name : '');
 
     const logoutUser = () => {
         logout();
+    };
+
+    const initialValues = {
+        oldPassword: '',
+        newPassword: '',
+    };
+
+    const [values, setValues] = useState(initialValues);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setValues({
+            ...values,
+            [name]: value,
+        });
+        console.log(values);
     };
 
     const avatarInputHandler = (event) => {
@@ -50,6 +68,42 @@ const Sidebar = () => {
         }
     }
 
+    const updatePasswordDetails = async () => {
+        if (values.oldPassword && values.oldPassword.trim() !== '' && values.newPassword && values.newPassword.trim() !== '') {
+            if (values.newPassword.length < 7) {
+                toast.error('Password should be atleast 7 characters long');
+            } else {
+                toast.info('Updating Password');
+                customLogger('Updating User Password');
+                const token = localStorage.getItem('userToken');
+                if (token) {
+                    await axios
+                        .patch(
+                            `${process.env.REACT_APP_BASE_API}/users/me/password`, { 
+                                oldPassword: values.oldPassword.trim(), 
+                                newPassword: values.oldPassword.trim()
+                            },
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${token}`,
+                                },
+                            }
+                        ).then(() => {
+                            toast.success('Password changed successfully');
+                            setValues(initialValues);
+                            setUpdatePasswordOpen(false);
+                        })
+                        .catch((err) => {
+                            httpErrorLogger(err);
+                            toast.error('Old Password is incorrect!');
+                        });
+                }
+            }
+        } else {
+            toast.error('Please enter old and new password');
+        }
+    };
+
     return (
         <>
             {isUserLoggedIn && (<SidebarStyle>
@@ -58,13 +112,19 @@ const Sidebar = () => {
                     src={source}
                     onDoubleClick={() => setUpdateProfileOpen(true)}
                 />
-                {isUpdateProfileOpen && <AvatarUpdater type='file' onChange={avatarInputHandler} /> }
+                {isUpdateProfileOpen && <AvatarUpdater type='file' onChange={avatarInputHandler} />}
                 <h2 onDoubleClick={() => setUpdateNameOpen(true)}>{userDetails.name}</h2>
                 {isUpdateNameOpen && <NameUpdateWrapper>
                     <input value={name} onChange={nameInputHandler} type="text" placeholder="Update Name" />
                     <button onClick={() => updateNameDetails()}>Update</button>
                 </NameUpdateWrapper>}
                 <p>{userDetails.email}</p>
+                {isUpdatePasswordOpen && <UpdatePasswordWrapper>
+                    <input value={values.oldPassword} onChange={handleInputChange} type="password" name="oldPassword" placeholder="Old Password" />
+                    <input value={values.newPassword} onChange={handleInputChange} type="password" name="newPassword" placeholder="New Password" />
+                    <button onClick={() => updatePasswordDetails()}>Update</button>
+                </UpdatePasswordWrapper> }
+                {!isUpdatePasswordOpen && <button onClick={() => setUpdatePasswordOpen(true)}>Change Password</button> }
                 <button onClick={logoutUser}>Logout</button>
             </SidebarStyle>)}
             <ToastContainer theme="dark" />
@@ -96,10 +156,12 @@ const SidebarStyle = styled.nav`
 
     h2 {
         margin-top: 16px;
+        cursor: pointer;
     };
 
     button {
-        margin-top: 22px;
+        width: 100%;
+        padding: 10px;
         background-color: white;
         color: black;
         border-radius: 5px;
@@ -120,6 +182,21 @@ const AvatarUpdater = styled.input`
 `;
 
 const NameUpdateWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+    align-items: center;
+
+    input {
+        width: 200px;
+        height: 30px;
+        border-radius: 5px;
+        border: none;
+        padding: 8px;
+    }
+`;
+
+const UpdatePasswordWrapper = styled.div`
     display: flex;
     flex-direction: column;
     gap: 8px;
